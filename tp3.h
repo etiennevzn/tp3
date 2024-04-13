ba5ffcbbd0886b087f7f140d5a353bb8ddf7e136
etiennevzn <166559256+etiennevzn@users.noreply.github.com>
2024-04-13 17:30:47 +0200

    Update tp3.h

--- tp3.h
+++ tp3.h
@@ -1,9 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-#include <stdio.h>
-#include <stdlib.h>
-
 typedef struct sommet sommet;
 typedef struct voisin voisin;
 typedef struct graphe graphe;
@@ -22,5 +19,20 @@ struct voisin{
 struct graphe{
     struct sommet* premier_sommet;
 }; 
+
+graphe* creerGraphe();
+void creerSommet(graphe *g, int id);
+sommet* rechercherSommet(graphe g, int id);
+int rechercherArete(graphe g, int id1, int id2);
+int ajouterVoisin(graphe *g, int id1, int id2);
+void ajouterArete(graphe *g, int id1, int id2);
+graphe* construireGraphe(int N);
+void afficherGraphe(graphe g);
+int rechercherDegre(graphe g);
+void supprimerSommet(graphe *g, int id);
+int nombreSommet(graphe g);
+int Cycle(sommet* sommetCurrent, int* visited, int parent, graphe g);
+int contientBoucle(graphe g);
+void fusionnerSommet(graphe *g, int idSommet1, int idSommet2);
 
 #endif
